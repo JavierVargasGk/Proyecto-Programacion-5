b546585f5d9959d6d5cf088c795140251f7d3b33
Javier <javiermvm6@gmail.com>
2025-03-31 20:16:22 -0600

    modificacion id de usuario de int autogenerable ( buscar a la hora de crear una cuenta generar un id de cuenta ya sea directo de firebase o creado y asociado a auth de firebase, con fines de hacer match y obtener las rutinas en el objeto usuario respectivo), fixed ejercicios teniendo un id unico, no es necesario un id por ejercicio

--- app/src/main/java/com/ulatina/proyectoprogra5/data/database/model/Ejercicios.kt
+++ app/src/main/java/com/ulatina/proyectoprogra5/data/database/model/Ejercicios.kt
@@ -3,7 +3,6 @@ package com.ulatina.proyectoprogra5.data.database.model
 
 
 data class Ejercicios(
-        val id : Long =0,
         val name : String = "",
         val reps : Long = 0,
         val isSelected : Boolean = false
